30870b8deec6b2c9872e6a6d6bf2b5361fe805fd
pablonara <pablonara@pablonara.com>
2024-06-18 18:21:37 -0700

    last hour of work

--- train.py
+++ train.py
@@ -15,8 +15,6 @@ rf = roboflow.Roboflow()
 project = rf.workspace("").project("")
 dataset = project.version().download("yolov8-obb") # bounding boxes
 
-
-
 with open(f'{dataset.location}/data.yaml', 'r') as file:
     data = yaml.safe_load(file)
 
@@ -28,3 +26,18 @@ with open(f'{dataset.location}/data.yaml', 'w') as file:
 model = YOLO('yolov8n-obb.pt')
 
 results = model.train(data=f"{dataset.location}/data.yaml", epochs=10, imgsz=640) # 10 passes at 640x640 resolution
+
+model = YOLO('runs/obb/train2/weights/best.pt')
+
+# testing here
+
+import os
+import random
+
+random_file = random.choice(os.listdir(f"{dataset.location}/test/images"))
+file_name = os.path.join(f"{dataset.location}/test/images", random_file)
+
+results = model(file_name)
+
+print(results[0])
+
